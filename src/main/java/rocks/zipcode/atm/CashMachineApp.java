7b1fbf49e385614409f2a2f30ca157903986f5fd
donnasvinis <dvsvinis@mac.com>
2019-06-08 07:16:51 -0400

    add Transaction page with buttons that go back and forth

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -1,5 +1,10 @@
 package rocks.zipcode.atm;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import rocks.zipcode.atm.bank.Bank;
 import javafx.application.Application;
 import javafx.scene.Parent;
@@ -18,20 +23,34 @@ public class CashMachineApp extends Application {
 
     private TextField field = new TextField();
     private CashMachine cashMachine = new CashMachine(new Bank());
+    private TextField name = new TextField();
+    private TextField email = new TextField();
+    private TextField amount = new TextField();
+    private TextField balance = new TextField();
+
 
     private Parent createContent() {
         VBox vbox = new VBox(10);
-        vbox.setPrefSize(600, 600);
+        vbox.setPrefSize(900, 900);
+
+
 
         TextArea areaInfo = new TextArea();
 
         Button btnSubmit = new Button("Set Account ID");
+        btnSubmit.setStyle("-fx-background-color: #ff0000; ");
+        btnSubmit.setMaxWidth(100);
+        btnSubmit.setPrefWidth(100);
         btnSubmit.setOnAction(e -> {
+
             int id = Integer.parseInt(field.getText());
             cashMachine.login(id);
 
             areaInfo.setText(cashMachine.toString());
         });
+
+
+
 
         Button btnDeposit = new Button("Deposit");
         btnDeposit.setOnAction(e -> {
@@ -56,7 +75,11 @@ public class CashMachineApp extends Application {
             areaInfo.setText(cashMachine.toString());
         });
 
+
+
         FlowPane flowpane = new FlowPane();
+        flowpane.setHgap(20);
+        flowpane.setVgap(20);
 
         flowpane.getChildren().add(btnSubmit);
         flowpane.getChildren().add(btnDeposit);
@@ -66,11 +89,27 @@ public class CashMachineApp extends Application {
         return vbox;
     }
 
+    /*******************************************************/
+
+
+    Scene login, accountPage;
+
+
     @Override
     public void start(Stage stage) throws Exception {
+        Label backToLogin = new Label("Back to Login");
+        Button toLogin = new Button("Back to Login");
+        toLogin.setOnAction(e -> stage.setScene(login));
+
         stage.setScene(new Scene(createContent()));
         stage.show();
+
+
     }
+
+
+
+
 
     public static void main(String[] args) {
         launch(args);
