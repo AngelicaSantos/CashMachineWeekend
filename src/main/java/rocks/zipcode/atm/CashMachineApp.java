5cd0e043024beb82d3eb8f0f29e594df141e53a4
donnasvinis <dvsvinis@mac.com>
2019-06-08 10:46:35 -0400

    Merge branch 'master' of https://github.com/AngelicaSantos/CashMachineWeekend into donna
    # Conflicts:
    #	src/main/java/rocks/zipcode/atm/CashMachineApp.java

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -10,6 +10,23 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.layout.FlowPane;
+//package rocks.zipcode.atm;
+
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import rocks.zipcode.atm.bank.Bank;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.FlowPane;
+import rocks.zipcode.atm.login.LoginComponent;
+
+import java.util.List;
 
 /**
  * @author ZipCodeWilmington
@@ -21,48 +38,15 @@ public class CashMachineApp extends Application {
 
     private Parent createContent() {
         VBox vbox = new VBox(10);
-        vbox.setPrefSize(900, 600);
+        vbox.setPrefSize(600, 600);
 
-        TextArea areaInfo = new TextArea();
+        //vbox.getChildren().addAll(field, flowpane, areaInfo);
 
-        Button btnSubmit = new Button("Set Account ID");
-        btnSubmit.setOnAction(e -> {
-            int id = Integer.parseInt(field.getText());
-            cashMachine.login(id);
+        LoginComponent loginComponent = new LoginComponent();
+        List<Node> list = loginComponent.getLoginElements();
+        Node[] nodes = list.toArray(new Node[]{});
+        vbox.getChildren().addAll(nodes);
 
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnDeposit = new Button("Deposit");
-        btnDeposit.setOnAction(e -> {
-            int amount = Integer.parseInt(field.getText());
-            cashMachine.deposit(amount);
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnWithdraw = new Button("Withdraw");
-        btnWithdraw.setOnAction(e -> {
-            int amount = Integer.parseInt(field.getText());
-            cashMachine.withdraw(amount);
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        Button btnExit = new Button("Exit");
-        btnExit.setOnAction(e -> {
-            cashMachine.exit();
-
-            areaInfo.setText(cashMachine.toString());
-        });
-
-        FlowPane flowpane = new FlowPane();
-
-        flowpane.getChildren().add(btnSubmit);
-        flowpane.getChildren().add(btnDeposit);
-        flowpane.getChildren().add(btnWithdraw);
-        flowpane.getChildren().add(btnExit);
-        vbox.getChildren().addAll(field, flowpane, areaInfo);
         return vbox;
     }
 
