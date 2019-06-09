4babd0951297b90895562e8512f193e4af98a60f
Erick Tepale <e.tepale@gmail.com>
2019-06-09 19:56:53 -0400

    changed data structs a bit

--- src/main/java/rocks/zipcode/atm/bank/BasicAccount.java
+++ src/main/java/rocks/zipcode/atm/bank/BasicAccount.java
@@ -4,7 +4,7 @@ package rocks.zipcode.atm.bank;
  * @author ZipCodeWilmington
  */
 public class BasicAccount extends Account {
-    private String type = "Basic";
+    public String type = "Basic";
 
     public BasicAccount(AccountData accountData) {
         super(accountData);
