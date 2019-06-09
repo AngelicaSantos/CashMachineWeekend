c8fd29cf028f85101e631246a4084ed7e68b5da3
AngelicaSantos <angelica.santos2113@gmail.com>
2019-06-09 11:24:40 -0400

    updated

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -1,15 +1,14 @@
 package rocks.zipcode.atm;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import rocks.zipcode.atm.bank.Bank;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.layout.FlowPane;
 //package rocks.zipcode.atm;
 
 import javafx.geometry.Pos;
@@ -25,6 +24,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.layout.FlowPane;
 import rocks.zipcode.atm.login.LoginComponent;
+import rocks.zipcode.atm.login.LoggedInComponent;
 
 import java.util.List;
 
@@ -35,26 +35,107 @@ public class CashMachineApp extends Application {
 
     private TextField field = new TextField();
     private CashMachine cashMachine = new CashMachine(new Bank());
+    Scene scene1, scene2;
 
-   /* This is Kavyas Login Screen */
-    private Parent createLogin() {
+    /* this is Kayva login screen */
+    private Parent createLogin(Stage stage, Scene scene1) {
         VBox vbox = new VBox(10);
         vbox.setPrefSize(600, 600);
-
         //vbox.getChildren().addAll(field, flowpane, areaInfo);
-
         LoginComponent loginComponent = new LoginComponent();
-        List<Node> list = loginComponent.getLoginElements();
+        List<Node> list = loginComponent.getLoginElements(stage, scene1, cashMachine);
         Node[] nodes = list.toArray(new Node[]{});
         vbox.getChildren().addAll(nodes);
-
         return vbox;
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createLogin()));
+
+     //Account field
+        Label labelId = new Label("Account ID");
+        TextField id = new TextField("Id should autopopulate");
+
+
+     //Name field
+        Label labelName = new Label("Name");
+        TextField name = new TextField("getName");
+
+        //Email field
+        Label labelEmail = new Label("Email");
+        TextField email = new TextField("getEmail");
+
+        //Balance field
+        Label labelBalance = new Label("Acccout Balance");
+        TextField balance = new TextField("getBalance");
+
+        //Transaction field
+        Label labelTransaction = new Label("Transaction Amount");
+        TextField transaction = new TextField();
+
+        //Account Type
+        TextField accountType = new TextField("actType");
+
+        //Button Deposit
+        Button btDeposit = new Button("Deposit");
+        btDeposit.setOnAction(e -> {
+            int amount = Integer.parseInt(transaction.getText());
+            cashMachine.deposit(amount);
+            balance.setText(String.valueOf(cashMachine.getBalance()));
+            System.out.println(cashMachine.toString());
+
+        });
+
+        //Button Withdraw
+        Button btWithdraw = new Button("Withdraw");
+        btWithdraw.setOnAction(e -> {
+            int amount = Integer.parseInt(transaction.getText());
+            cashMachine.withdraw(amount);
+            balance.setText(String.valueOf(cashMachine.getBalance()));
+            System.out.println(cashMachine.toString());
+
+        });
+
+
+        //Button Login
+        Button btLogin = new Button("Logout");
+
+
+    //Grid layout
+        GridPane.setConstraints(labelId, 0 , 0);
+        GridPane.setConstraints(id, 1, 0);
+        GridPane.setConstraints(labelName, 0 , 1);
+        GridPane.setConstraints(name, 1, 1);
+        GridPane.setConstraints(labelEmail, 0 , 2);
+        GridPane.setConstraints(email, 1, 2);
+        GridPane.setConstraints(labelBalance, 0 , 3);
+        GridPane.setConstraints(balance, 1, 3);
+        GridPane.setConstraints(labelTransaction, 0 , 5);
+        GridPane.setConstraints(transaction, 1, 5);
+        GridPane.setConstraints(btDeposit, 3, 4);
+        GridPane.setConstraints(btWithdraw, 3, 5);
+        GridPane.setConstraints(btLogin, 3, 8);
+        GridPane.setConstraints(accountType, 3, 0);
+
+     //Layout accountPage
+        GridPane accountPage = new GridPane();
+        accountPage.setBackground(new Background(new BackgroundFill(Color.LIGHTSEAGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+        accountPage.setPadding(new Insets(10, 10, 10, 10));
+        accountPage.getChildren().addAll(btLogin, labelId, id, name, labelName, labelEmail, email,
+                labelTransaction, transaction, labelBalance, balance, btDeposit, btWithdraw, accountType);
+        accountPage.setVgap(10);
+        accountPage.setHgap(10);
+
+        scene2 = new Scene(accountPage, 580, 350);
+        scene1 =  new Scene(createLogin(stage, scene2));
+
+        //logout button action
+        btLogin.setOnAction(e -> stage.setScene(scene1));
+        stage.setTitle("Dragon Bank United");
+        stage.setResizable(false);
+        stage.setScene(scene1);
         stage.show();
+
     }
 
     public static void main(String[] args) {
